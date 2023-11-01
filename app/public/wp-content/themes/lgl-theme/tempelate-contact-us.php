14d274a134bfc9423d8ef788bd6458fc746cd5d4
RammeDon <raminkhareji@gmail.com>
2023-11-01 12:28:58 +0100

    Adding singular files for each page and post type

---
+++ app/public/wp-content/themes/lgl-theme/tempelate-contact-us.php
@@ -0,0 +1,6 @@
+<?php
+/* Template Name: Contact Us */
+// This is the Contact Us page
+?>  
+
+<h1>This is Contact Us page</h1>
---
+++ app/public/wp-content/themes/lgl-theme/tempelate-products-page.php
@@ -0,0 +1,6 @@
+<?php
+/* Template Name: Products Page */
+// This is the products page
+?>
+
+<h1>This is the products page</h1>
