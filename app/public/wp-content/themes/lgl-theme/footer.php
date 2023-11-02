b077fe3d5838870a121bc16439180b8436fecb4c
RammeDon <raminkhareji@gmail.com>
2023-11-02 23:13:03 +0100

    Updated styling on footer and header

--- app/public/wp-content/themes/lgl-theme/footer.php
+++ app/public/wp-content/themes/lgl-theme/footer.php
@@ -1,29 +1,28 @@
-        <hr> <!-- remove this in the end -->
-        <footer>
+        <footer class="footer-container">
             <div class="footer-content">
                 <div class="logo-section">
                     <!-- <a href="https://www.linkedin.com/in/ramin-darudi-382b0a207/">
                         LinkedIn
                     </a> -->
-                    <img src="<?php echo get_template_directory_uri(); ?>/images/LGL-Logo.png" alt="Business Logo">
+                    <img width="300" height="150" src="<?php echo get_template_directory_uri(); ?>/images/LGL-Logo-White.png" alt="Business Logo">
                 </div>
 
                 <div class="contact-info">
                     <strong>Contact Info:</strong>
-                    <p class="sub-info">Email: <a href="mailto:example@gmail.com">example@gmail.com</a></p>
-                    <p class="phone-number sub-info">Phone number: 0707070707</p>
+                    <p class="sub-info"><i class="fa-solid fa-envelope contact-icon" style="color: #ffffff;"></i><a href="mailto:example@gmail.com">example@gmail.com</a></p>
+                    <p class="phone-number sub-info"><i class="fa-solid fa-phone contact-icon" style="color: #ffffff;"></i>0707070707</p>
                 </div>
 
                 <div class="location-section">
                     <strong>Locations:</strong>
-                    <p class="sub-info"><a href="https://www.google.com/maps/place/Skifferv%C3%A4gen+23,+224+78+Lund/@55.692876,13.2138541,17z/data=!4m15!1m8!3m7!1s0x465397bd79a52c1f:0xd984c03193aa1ff0!2sSkifferv%C3%A4gen+23,+224+78+Lund!3b1!8m2!3d55.692876!4d13.2164344!16s%2Fg%2F11c43v7z99!3m5!1s0x465397bd79a52c1f:0xd984c03193aa1ff0!8m2!3d55.692876!4d13.2164344!16s%2Fg%2F11c43v7z99?entry=ttu">
-                        Office: Skiffervägen 23, 22478, Lund, Sweden</a></p>
+                    <p class="sub-info">Office: <a href="https://www.google.com/maps/place/Skifferv%C3%A4gen+23,+224+78+Lund/@55.692876,13.2138541,17z/data=!4m15!1m8!3m7!1s0x465397bd79a52c1f:0xd984c03193aa1ff0!2sSkifferv%C3%A4gen+23,+224+78+Lund!3b1!8m2!3d55.692876!4d13.2164344!16s%2Fg%2F11c43v7z99!3m5!1s0x465397bd79a52c1f:0xd984c03193aa1ff0!8m2!3d55.692876!4d13.2164344!16s%2Fg%2F11c43v7z99?entry=ttu">
+                        Skiffervägen 23, 22478, Lund, Sweden</a></p>
                     <p class="sub-info">Postal address: Kronetorps alle 19c, 23239, Arlöv, Sweden</p>
                 </div>
                 
             </div>
             <div class="powered-by">
-                <p>Powered by ReckonTech</p>
+                <p><i class="fa-regular fa-copyright"></i> Powered by ReckonTech</p>
             </div>
         </footer>
 
--- app/public/wp-content/themes/lgl-theme/header.php
+++ app/public/wp-content/themes/lgl-theme/header.php
@@ -13,13 +13,13 @@
     <nav class="header-navbar">
         <div class="navbar">
             <a href="<?php echo site_url() ?>">
-                <img width="150" height="100" src="<?php echo get_template_directory_uri(); ?>/images/LGL-New-Logo.png" alt="Business Logo">
+                <img width="150" height="90" src="<?php echo get_template_directory_uri(); ?>/images/LGL-New-Logo.png" alt="Business Logo">
             </a>
             
             <div class="blog-products">
                 <div class="dropdown navbar-item"> <!-- Added class "dropdown" here -->
                     <div class="product-container">
-                        <p class="product-dropdown">Products</p>
+                        <p class="product-dropdown navbar-item">Products</p>
                         <!-- <svg class="dropdown-icon" xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 320 512">! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc.<path d="M137.4 374.6c12.5 12.5 32.8 12.5 45.3 0l128-128c9.2-9.2 11.9-22.9 6.9-34.9s-16.6-19.8-29.6-19.8L32 192c-12.9 0-24.6 7.8-29.6 19.8s-2.2 25.7 6.9 34.9l128 128z"/></svg> -->
                         <i class="fa-solid fa-chevron-down fa-fade dropdown-icon" style="color: #511f46;"></i>
                     </div>
@@ -52,7 +52,7 @@
                     </div>
                 </div>
                 <a class="navbar-item" href="<?php echo site_url("/blogs-page") ?>">Blog</a>
-                <a class="contact-us" href="<?php echo site_url("/contact-us-page") ?>">Contact Us</a>
+                <a class="contact-us navbar-item" href="<?php echo site_url("/contact-us-page") ?>">Contact Us</a>
             </div>
         </div>
         <div class="navbar">
