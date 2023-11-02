7f9eb637525e4b70e41b4984febebe9e93a032b8
RammeDon <raminkhareji@gmail.com>
2023-11-02 21:45:59 +0100

    Added additional styling for header and footer

--- app/public/wp-content/themes/lgl-theme/footer.php
+++ app/public/wp-content/themes/lgl-theme/footer.php
@@ -7,18 +7,18 @@
                     </a> -->
                     <img src="<?php echo get_template_directory_uri(); ?>/images/LGL-Logo.png" alt="Business Logo">
                 </div>
+
                 <div class="contact-info">
-                    <div class="contact-section">
-                        <strong>Contact Info:</strong>
-                        <p>Email: <a href="mailto:example@gmail.com">example@gmail.com</a></p>
-                        <p>Phone number: 0707070707</p>
-                    </div>
-                    <div class="location-section">
-                        <strong>Locations:</strong>
-                        <p><a href="https://www.google.com/maps/place/Skifferv%C3%A4gen+23,+224+78+Lund/@55.692876,13.2138541,17z/data=!4m15!1m8!3m7!1s0x465397bd79a52c1f:0xd984c03193aa1ff0!2sSkifferv%C3%A4gen+23,+224+78+Lund!3b1!8m2!3d55.692876!4d13.2164344!16s%2Fg%2F11c43v7z99!3m5!1s0x465397bd79a52c1f:0xd984c03193aa1ff0!8m2!3d55.692876!4d13.2164344!16s%2Fg%2F11c43v7z99?entry=ttu">
-                            Office: Skiffervägen 23, 22478, Lund, Sweden</a></p>
-                        <p>Postal address: Kronetorps alle 19c, 23239, Arlöv, Sweden</p>
-                    </div>
+                    <strong>Contact Info:</strong>
+                    <p class="sub-info">Email: <a href="mailto:example@gmail.com">example@gmail.com</a></p>
+                    <p class="phone-number sub-info">Phone number: 0707070707</p>
+                </div>
+
+                <div class="location-section">
+                    <strong>Locations:</strong>
+                    <p class="sub-info"><a href="https://www.google.com/maps/place/Skifferv%C3%A4gen+23,+224+78+Lund/@55.692876,13.2138541,17z/data=!4m15!1m8!3m7!1s0x465397bd79a52c1f:0xd984c03193aa1ff0!2sSkifferv%C3%A4gen+23,+224+78+Lund!3b1!8m2!3d55.692876!4d13.2164344!16s%2Fg%2F11c43v7z99!3m5!1s0x465397bd79a52c1f:0xd984c03193aa1ff0!8m2!3d55.692876!4d13.2164344!16s%2Fg%2F11c43v7z99?entry=ttu">
+                        Office: Skiffervägen 23, 22478, Lund, Sweden</a></p>
+                    <p class="sub-info">Postal address: Kronetorps alle 19c, 23239, Arlöv, Sweden</p>
                 </div>
                 
             </div>
--- app/public/wp-content/themes/lgl-theme/header.php
+++ app/public/wp-content/themes/lgl-theme/header.php
@@ -9,42 +9,52 @@
 
     <body <?php body_class(); ?>>
     <header>
-        <nav class="header-navbar">
-            <ul class="navbar-items">
-                <li><a href="<?php echo site_url() ?>">Home</a></li>
-                <li class="dropdown"> <!-- Added class "dropdown" here -->
+    <nav class="header-navbar">
+        <div class="navbar">
+            <a href="<?php echo site_url() ?>">
+                <img src="<?php echo get_template_directory_uri(); ?>/images/LGL-Logo-removebg.png" alt="Business Logo">
+            </a>
+            
+            <div class="blog-products">
+                <div class="dropdown navbar-item"> <!-- Added class "dropdown" here -->
                     Products
                     <div class="dropdown-content">
-                        <div class="row">
-                            <div class="column">
-                                <div class="category">Devices</div>
-                                <div class="item">Item 1</div>
-                                <div class="item">Item 2</div>
-                            </div>
-                            <div class="column">
-                                <div class="category">Softwares</div>
-                                <div class="item">Item 1</div>
-                                <div class="item">Item 2</div>
-                            </div>
+                    <div class="row">
+                        <div class="column">
+                            <div class="category">Devices</div>
+                            <div class="item dropdown-text">Item 1</div>
+                            <div class="item dropdown-text">Item 2</div>
                         </div>
-                        <div class="row">
-                            <div class="column">
-                                <div class="category">Lab Equipment</div>
-                                <div class="item">Item 1</div>
-                                <div class="item">Item 2</div>
-                            </div>
-                            <div class="column">
-                                <div class="category">Optomechanics</div>
-                                <div class="item">Item 1</div>
-                                <div class="item">Item 2</div>
-                            </div>
+                        <div class="column">
+                            <div class="category">Softwares</div>
+                            <div class="item dropdown-text">Item 1</div>
+                            <div class="item dropdown-text">Item 2</div>
                         </div>
                     </div>
-                </li>
-                <li><a href="<?php echo site_url("/contact-us-page") ?>">Contact Us</a></li>
-                <li><a href="<?php echo site_url("/blogs-page") ?>">Blog</a></li>
-            </ul>
-        </nav>
+                    <div class="row">
+                        <div class="column">
+                            <div class="category">Lab Equipment</div>
+                            <div class="item dropdown-text">Item 1</div>
+                            <div class="item dropdown-text">Item 2</div>
+                        </div>
+                        <div class="column">
+                            <div class="category">Optomechanics</div>
+                            <div class="item dropdown-text">Item 1</div>
+                            <div class="item dropdown-text">Item 2</div>
+                        </div>
+                    </div>
+                    </div>
+                </div>
+                <a class="navbar-item" href="<?php echo site_url("/blogs-page") ?>">Blog</a>
+                <a class="contact-us" href="<?php echo site_url("/contact-us-page") ?>">Contact Us</a>
+            </div>
+        </div>
+        <div class="navbar">
+            <a class="navbar-item sign-in" href="#">Sign In</a>
+            
+        </div>
+    </nav>
+
     </header>
     <hr> <!-- remove this in the end -->
-</body>
+    </body>
