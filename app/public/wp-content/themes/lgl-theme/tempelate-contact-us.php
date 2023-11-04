165aebe3ed48bf82c3c0e29983d94ae8e4603b26
RammeDon <raminkhareji@gmail.com>
2023-11-04 19:24:23 +0100

    Fixed more styling

--- app/public/wp-content/themes/lgl-theme/tempelate-contact-us.php
+++ app/public/wp-content/themes/lgl-theme/tempelate-contact-us.php
@@ -11,23 +11,29 @@
     <section>
         <form class="contact-us-form" method="post" action="process-contact-form.php">
             <h1 class="page-title">Contact Us</h1>
-            <div class="form-group">
+
+            <div class="name-section">
                 <label for="full-name">Full Name:</label>
                 <input type="text" class="full-name" name="full-name" required>
             </div>
-            <div class="form-group">
+
+            <div class="email-section">
+                <!-- Make it so that when a user is logged in the email auto fills -->
                 <label for="email">Email:</label>
                 <input type="email" class="email" name="email" required>
             </div>
-            <div class="form-group">
+
+            <div class="pNumber-section">
                 <label for="phone">Phone Number (optional):</label>
                 <input type="tel" class="phone" name="phone">
             </div>
-            <div class="form-group">
+
+            <div class="subject-section">
                 <label for="subject">Subject:</label>
                 <input type="text" class="subject" name="subject" required>
             </div>
-            <div class="form-group">
+
+            <div class="message-section">
                 <label for="message">Message:</label>
                 <textarea class="message" name="message" rows="6" required></textarea>
             </div>
