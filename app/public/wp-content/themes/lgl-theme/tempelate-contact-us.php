68168908ccc0767de02610ee669a785d7e8cc6a7
RammeDon <raminkhareji@gmail.com>
2023-11-04 17:46:37 +0100

    starting contact us php and css

--- app/public/wp-content/themes/lgl-theme/tempelate-contact-us.php
+++ app/public/wp-content/themes/lgl-theme/tempelate-contact-us.php
@@ -7,9 +7,34 @@
     get_header();
  ?>
 
-<div>
-    <h1>This is Contact Us</h1>
-</div>
+<main class="contact-us-container">
+    <section>
+        <form class="contact-us-form" method="post" action="process-contact-form.php">
+            <h1 class="page-title">Contact Us</h1>
+            <div class="form-group">
+                <label for="full-name">Full Name:</label>
+                <input type="text" class="full-name" name="full-name" required>
+            </div>
+            <div class="form-group">
+                <label for="email">Email:</label>
+                <input type="email" class="email" name="email" required>
+            </div>
+            <div class="form-group">
+                <label for="phone">Phone Number (optional):</label>
+                <input type="tel" class="phone" name="phone">
+            </div>
+            <div class="form-group">
+                <label for="subject">Subject:</label>
+                <input type="text" class="subject" name="subject" required>
+            </div>
+            <div class="form-group">
+                <label for="message">Message:</label>
+                <textarea class="message" name="message" rows="6" required></textarea>
+            </div>
+            <button type="submit">Submit</button>
+        </form>
+    </section>
+</main>
 
 <?php
     get_footer();
