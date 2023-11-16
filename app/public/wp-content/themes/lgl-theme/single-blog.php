bdf9965382f5f1b67b4146101adf0f6ba891084f
RammeDon <raminkhareji@gmail.com>
2023-11-16 19:01:19 +0100

    Developing blog page

--- app/public/wp-content/themes/lgl-theme/single-blog.php
+++ app/public/wp-content/themes/lgl-theme/single-blog.php
@@ -2,10 +2,33 @@
 <?php
     get_header();
  ?>
+ <section class="container">
     
     
-    <?php the_content(); ?>
+    <?php 
+    $content = get_the_content();
+    
+    // Extract image URLs
+    $imageUrls = [];
+    preg_match_all('/<img src="([^"]*)" alt="([^"]*)" \/>/', $content, $matches);
+    foreach ($matches[1] as $imageUrl) {
+        $imageUrl = '<img src="' . $imageUrl . '" alt="" />';
+        array_push($imageUrls, $imageUrl);
+    }
+    
+    // Extract text content
+    $textContent = preg_replace('/<img src="([^"]*)" alt="([^"]*)" \/>/', '', $content);
+    
+    // Display images
+    foreach ($imageUrls as $imageUrl) {
+        echo $imageUrl;
+    }
+    
+    // Display text
+    echo '<p>' . $textContent . '</p>';
+     ?>
 
+</section>
 <?php
     get_footer();
  ?>
