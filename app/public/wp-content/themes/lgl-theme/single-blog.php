a4ff528fbde2b7d5a2bb78199f542dbaffed2c4d
RammeDon <raminkhareji@gmail.com>
2023-11-16 19:59:51 +0100

    Developing topic

--- app/public/wp-content/themes/lgl-theme/single-blog.php
+++ app/public/wp-content/themes/lgl-theme/single-blog.php
@@ -6,8 +6,21 @@
     
     
     <?php 
+
+    $topic = get_the_title();
     $content = get_the_content();
+    ?>
+    <title><?php echo $topic ?></title>
+    <section class="blog-topic">
+        
+        <h1><?php echo $topic ?></h1>
+        <!-- <div class="sub-info">
+            <time></time>
+        </div> -->
+    </section>
     
+    
+    <?php 
     // Extract image URLs
     $imageUrls = [];
     preg_match_all('/<img src="([^"]*)" alt="([^"]*)" \/>/', $content, $matches);
