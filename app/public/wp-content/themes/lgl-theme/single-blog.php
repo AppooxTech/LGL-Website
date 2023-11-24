f9d5466ef7bb6dcf180ae003cad5cebd618bf4f1
RammeDon <raminkhareji@gmail.com>
2023-11-24 23:25:13 +0100

    Updating related blogs section

--- app/public/wp-content/themes/lgl-theme/single-blog.php
+++ app/public/wp-content/themes/lgl-theme/single-blog.php
@@ -11,17 +11,18 @@
     
     
     <?php 
-
+    $post_date = get_the_time('jS M Y');
     $topic = get_the_title();
     $tags = get_the_tags();
     $author = get_the_author();
     $content = get_the_content();
+    $tags_filter = ""
     ?>
     <title><?php echo $topic ?></title>
     <section class="blog-topic">
         <h1> <?php echo $topic ?></h1>
         <div class="sub-topic-info">
-            <time>26th Oct 2023</time>
+            <time><?php echo $post_date ?></time>
             <p>Author: <?php echo $author ?></p>
             <p>Tags: </p>
             <tags>
@@ -29,7 +30,9 @@
                 $tags_count = count($tags);
                 foreach ($tags as $index => $tag) {
                     echo $tag->name;
+                    $tags_filter .= $tag->name;
                     if ($index < $tags_count - 1) {
+                        $tags_filter .= ",";
                         echo ', ';
                     }
                 }
@@ -65,32 +68,27 @@
         </div>
 
         <?php 
-            $related_blogs = new WP_Query(array( 'tag' => "announcement" ))
+            $related_blogs = new WP_Query(array( 'tag' => $tags_filter ))
 
          ?>
 
         <div class="related-blog-container">
             <?php 
+                $related_counter= 0;
                 if ($related_blogs->have_posts()) {
                     while ($related_blogs->have_posts()) {
+                        $related_counter++;
                         $related_blogs->the_post();
-                        if (get_the_title() != $topic){
-                            $counter = 1;
-                            $imageUrls_sub = [];
-                            preg_match_all('/<img src="([^"]*)" alt="([^"]*)" \/>/', $content, $matches_sub);
-                            foreach ($matches[1] as $imageUrl) {
-                                $imageUrl = '<img src="' . $imageUrl . '" alt="" />';
-                                array_push($imageUrls_sub, $imageUrl);
-                            }
+                        if (get_the_title() != $topic && $related_counter < 5){
                             ?>
                                 <div class="related-blogs">
                                     <h2><?php echo get_the_title(); ?></h2>
-                                    <?php
-                                        foreach ($imageUrls_sub as $imageUrl) {
-                                            echo $imageUrl;
-                                        }
-                                    
-                                    ?>
+                                    <div class="date-author">
+                                        <p><?php echo get_the_time('jS M Y'); ?></p>
+                                        <p><?php echo get_the_author(); ?></p>
+                                        <p><?php echo $tags_filter ?></p>
+                                    </div>
+
                                 </div>
                                 
                             <?php 
