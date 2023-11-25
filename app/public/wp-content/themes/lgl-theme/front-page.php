c77517d0204688444fda15131272b853089c0716
Kurosh Zhalerajabi <kjreem.imagine@gmail.com>
2023-11-25 18:52:58 +0330

    [fix]header,front-page: fixed header overlay issue

--- app/public/wp-content/themes/lgl-theme/front-page.php
+++ app/public/wp-content/themes/lgl-theme/front-page.php
@@ -6,9 +6,7 @@ $carousel_contents = [['header' => 'HYSPIM', 'description' => 'HYSPIM DESC', 'im
 ?>
 
 <div class="front-page-container">
-    <section class="header-container">
-        <?php get_header(); ?>
-    </section>
+    <?php get_header(); ?>
     <section class="body-container">
         <!-- <?php get_template_part("components/carousel", '', ['contents' => $carousel_contents]); ?> -->
         <div class="aboutus-container aboutus-background">
@@ -29,39 +27,36 @@ $carousel_contents = [['header' => 'HYSPIM', 'description' => 'HYSPIM DESC', 'im
             </p>
         </div>
         <div class="services-container services-background">
-                <section class="services-first">
-                    <h1 class="services">
-                        services
-                    </h1>
-                    <p class="services">
-                        Our expertise is in the fields of optical engineering, optical design, and prototyping
-                        optomechanical instruments.
-                        Light Guide in Lund AB (LGL AB) is involved in a range of activities including Optics and
-                        Opto-Mechanics, designing and
-                        manufacturing of instruments and procurement of instruments, design, and manufacturing of vision
-                        systems, and milling & machining services.
-                    </p>
-                </section>
-                    <h2 class="services">
-                        LGL AB provides services for universities and industries such as
-                    </h2>
-                    <ul class="services">
-                        <li>Prototyping optomechanical systems.</li>
-                        <li>Vision system design.</li>
-                        <li>High-Quality Reliable CNC Machining Service at Low Cost</li>
-                        <li>Design of optical systems using Zemax software.</li>
-                        <li>Consulting in setting up the educational optical laboratories, undergraduate and graduate
-                            labs.
-                        </li>
-                        <li>Consulting in the procurement of laboratory instruments and materials.</li>
-                    </ul>
-                </section>
-            </div>
-            <div class="footer-container">
-                <?php get_footer(); ?>
-            </div>
+            <section class="services-first">
+                <h1 class="services">
+                    services
+                </h1>
+                <p class="services">
+                    Our expertise is in the fields of optical engineering, optical design, and prototyping
+                    optomechanical instruments.
+                    Light Guide in Lund AB (LGL AB) is involved in a range of activities including Optics and
+                    Opto-Mechanics, designing and
+                    manufacturing of instruments and procurement of instruments, design, and manufacturing of vision
+                    systems, and milling & machining services.
+                </p>
+            </section>
+            <h2 class="services">
+                LGL AB provides services for universities and industries such as
+            </h2>
+            <ul class="services">
+                <li>Prototyping optomechanical systems.</li>
+                <li>Vision system design.</li>
+                <li>High-Quality Reliable CNC Machining Service at Low Cost</li>
+                <li>Design of optical systems using Zemax software.</li>
+                <li>Consulting in setting up the educational optical laboratories, undergraduate and graduate
+                    labs.
+                </li>
+                <li>Consulting in the procurement of laboratory instruments and materials.</li>
+            </ul>
     </section>
-
+    <section class="footer-container">
+        <?php get_footer(); ?>
+    </section>
 </div>
 
 <style>
