d582e540f3d44ec345f6615f186dc3df9965f37c
RammeDon <raminkhareji@gmail.com>
2023-11-26 04:24:32 +0100

    fixing css assignment for blog view page

--- app/public/wp-content/themes/lgl-theme/functions.php
+++ app/public/wp-content/themes/lgl-theme/functions.php
@@ -93,6 +93,7 @@ function filter_posts() {
     );
 
     $args_filtered = array(
+
         'category_name' => 'blog',
         'tag' => $tag,
         'post_type' => 'post',
@@ -190,3 +191,4 @@ add_action('wp_ajax_nopriv_filter_posts', 'filter_posts');
 add_action('wp_enqueue_scripts', 'enqueue_jquery');
 add_action('wp_ajax_filter_posts', 'filter_posts');
 add_action('wp_ajax_nopriv_filter_posts', 'filter_posts');
+
