1d9fa6c89a7ad3b07b8089eb7e4026e9fa636dc7
Kurosh Zhalerajabi <kjreem.imagine@gmail.com>
2023-11-29 10:51:06 +0330

    [feat]carousel, button: created carousel basic template + added custom class to button

--- app/public/wp-content/themes/lgl-theme/components/carousel.php
+++ app/public/wp-content/themes/lgl-theme/components/carousel.php
@@ -1,29 +1,36 @@
-<?php 
-    $contents = [];
+<?php
+$contents = [];
 
-    if(isset($args) && isset($contents)){
-        $contets = $args['contents'];
-    }
+if (isset($args) && isset($contents)) {
+    $contets = $args['contents'];
+}
 ?>
 
 <div class="carousel-container">
-    <?php foreach($contets as $content): ?>
-    <div class="carousel-view">
+    <?php foreach ($contets as $content): ?>
         <button class="carousel-btn">
-            <img src="<?php echo get_template_directory_uri(); ?>/images/icons/icons8-chevron-left-50" alt="left">
+            <img src="<?php echo get_template_directory_uri(); ?>/images/icons/icons8-chevron-left-50.png" alt="left">
         </button>
-        <div class="carousel-content">
-            <div class="content-description-container">
-                <h2 class="content-description-heade"><?php $content['name'] ?></h2>
-                <p class="content-description-body"><?php $content['description']; ?></p>
-            </div>
-            <div class="content-image-container">
-                <img src="<? echo $content['image']; ?>" alt="Header">
+        <div class="carousel-view">
+            <div class="carousel-contents-container">
+                <div class="carousel-single-content-container">
+                    <div class="carousel-content-description-container">
+                        <h2 class="carousel-content-description-header">
+                            <?php echo esc_html($content['name']); ?>
+                        </h2>
+                        <p class="carousel-content-description-body">
+                            <?php echo esc_html($content['description']); ?>
+                        </p>
+                        <?php get_template_part('components/button', 'carousel_content_button', ['button_txt' => 'View Product', 'custom_class' => 'carousel-content-button']); ?>
+                    </div>
+                    <div class="carousel-content-image-container">
+                        <img src="<? echo $content['image']; ?>" alt="Header">
+                    </div>
+                </div>
             </div>
         </div>
         <button class="carousel-btn">
-            <img src="<?php echo get_template_directory_uri(); ?>/images/icons/icons8-chevron-right-50" alt="right">
+            <img src="<?php echo get_template_directory_uri(); ?>/images/icons/icons8-chevron-right-50.png" alt="right">
         </button>
-    </div>
     <?php endforeach; ?>
 </div>
