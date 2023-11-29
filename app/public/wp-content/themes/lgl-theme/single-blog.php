f718899afa78ab59de09f7afd68702933a549132
RammeDon <raminkhareji@gmail.com>
2023-11-29 02:15:59 +0100

    new blog view styling

--- app/public/wp-content/themes/lgl-theme/single-blog.php
+++ app/public/wp-content/themes/lgl-theme/single-blog.php
@@ -16,10 +16,37 @@
     $tags = get_the_tags();
     $author = get_the_author();
     $content = get_the_content();
-    $tags_filter = ""
+    $tags_filter = "";
+
+    $dom = new DOMDocument;
+    libxml_use_internal_errors(true);
+    $dom->loadHTML(mb_convert_encoding($content, 'HTML-ENTITIES', 'UTF-8'), LIBXML_HTML_NOIMPLIED | LIBXML_HTML_NODEFDTD);
+    libxml_clear_errors();
+    // $images = $dom->getElementsByTagName('img');
+    // $paragraphs = $dom->getElementsByTagName('p');
+
+    $images = $dom->getElementsByTagName('img');
+    $figures = $dom->getElementsByTagName('figure');
+    if ($images->length > 0) {
+        $firstImage = $images->item(0);
+        $firstFigure = $figures->item(0);
+        // Get existing classes
+        $existingClasses = $firstImage->getAttribute('class');
+        // Append new class 'cover-img'
+        $newClasses = $existingClasses . ' cover-img';
+        $firstImage->setAttribute('class', $newClasses);
+        $firstImage->parentNode->removeChild($firstImage);
+        $firstFigure->parentNode->removeChild($firstFigure);
+
+    }
+
+    $modifiedContent = $dom->saveHTML();
+
     ?>
     <title><?php echo $topic ?></title>
     <section class="blog-topic">
+        <div><?php echo $dom->saveHTML($firstImage); ?></div>
+        
         <h1> <?php echo $topic ?></h1>
         <div class="sub-topic-info">
             <time><?php echo $post_date ?></time>
@@ -46,24 +73,29 @@
     
         <div class="content-container">
             <?php 
+                // $sub_content = get_the_content();
+
+
+
+
             // Extract image URLs
-            $imageUrls = [];
-            preg_match_all('/<img src="([^"]*)" alt="([^"]*)" \/>/', $content, $matches);
-            foreach ($matches[1] as $imageUrl) {
-                $imageUrl = '<img src="' . $imageUrl . '" alt="" />';
-                array_push($imageUrls, $imageUrl);
-            }
+            // $imageUrls = [];
+            // preg_match_all('/<img src="([^"]*)" alt="([^"]*)" \/>/', $content, $matches);
+            // foreach ($matches[1] as $imageUrl) {
+            //     $imageUrl = '<img src="' . $imageUrl . '" alt="" />';
+            //     array_push($imageUrls, $imageUrl);
+            // }
             
-            // Extract text content
-            $textContent = preg_replace('/<img src="([^"]*)" alt="([^"]*)" \/>/', '', $content);
+            // // Extract text content
+            // $textContent = preg_replace('/<img src="([^"]*)" alt="([^"]*)" \/>/', '', $content);
             
-            // Display images
-            foreach ($imageUrls as $imageUrl) {
-                echo $imageUrl;
-            }
+            // // Display images
+            // foreach ($imageUrls as $imageUrl) {
+            //     echo $imageUrl;
+            // }
             
             // Display text
-            echo '<p>' . $textContent . '</p>';
+            echo '<p>' . $modifiedContent . '</p>';
             ?>
         </div>
     </section>
