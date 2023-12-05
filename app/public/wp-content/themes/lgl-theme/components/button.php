ea3585ecdb69d5daf029d4fd46aa192b0d0c17a7
Kurosh Zhalerajabi <kjreem.imagine@gmail.com>
2023-12-06 00:31:44 +0330

    [feat]custom-button: added random id to buttons

--- app/public/wp-content/themes/lgl-theme/components/button.php
+++ app/public/wp-content/themes/lgl-theme/components/button.php
@@ -1,37 +1,44 @@
 <?php
+// include('C:\Users\kurosh\Local Sites\LGL-Website\app\public\wp-content\themes\lgl-theme\components\utils\random_hex_generator.php
 $button_txt = 'Button';
 $on_click = null;
 $custom_class = '';
+$button_id = generate_random_hex();
 
-if (isset($args)) {
-    if (isset($args['button_txt'])) {
+if(isset($args)) {
+    if(isset($args['button_txt'])) {
         $button_txt = $args['button_txt'];
     }
-    if (isset($args['on_click'])) {
+    if(isset($args['on_click'])) {
         $on_click = $args['on_click'];
     }
-    if (isset($args['custom_class'])) {
+    if(isset($args['custom_class'])) {
         $custom_class = $args['custom_class'];
     }
 }
 ?>
 
-<button id="custom-button" class="<?php echo $custom_class; ?>" onmousedown="on_mouse_down()" onmouseup="on_mouse_up()" onclick="<?php ($on_click) ? call_user_func($on_click) : '' ?>"><?php echo esc_html($button_txt) ?></button>
+<button id="<?php echo $button_id; ?>" class="<?php echo $custom_class; ?> custom-button"
+    onmousedown="on_mouse_down('<?php echo $button_id; ?>')" onmouseup="on_mouse_up('<?php echo $button_id; ?>')"
+    onclick="<?php ($on_click) ? call_user_func($on_click) : '' ?>">
+    <?php echo esc_html($button_txt) ?>
+</button>
 
 <script>
-    const button = document.getElementById('custom-button');
-
-    function on_mouse_down() {
+    function on_mouse_down(id) {
+        const button = document.getElementById(id);
         button.classList.add('clicked');
+        console.log(button);
     }
 
-    function on_mouse_up() {
+    function on_mouse_up(id) {
+        const button = document.getElementById(id);
         button.classList.remove('clicked');
     }
 </script>
 
 <style>
-    button#custom-button {
+    button.custom-button {
         outline: 2px solid var(--dark);
         border: 2px solid var(--dark);
         background-color: var(--light);
@@ -43,7 +50,7 @@ if (isset($args)) {
         font-size: x-large;
     }
 
-    button#custom-button:hover {
+    button.custom-button:hover {
         outline-width: 4px;
         outline-color: var(--dark);
         border-color: var(--light);
@@ -51,7 +58,7 @@ if (isset($args)) {
         background-color: var(--dark);
     }
 
-    button#custom-button.clicked {
+    button.custom-button.clicked {
         border-width: 6px;
     }
 </style>
